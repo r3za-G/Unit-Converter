7210446b75557d8986fac0667ac7376433775739
Reza Gharooni <reazgharooni@outlook.com>
2023-01-16 17:30:09 +0000

    Initial commit

---
+++ Unit Converter/Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Reza Gharooni on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
